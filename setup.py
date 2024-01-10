dc01c16d7d319f3942e7ef497899a77245ce9473
Mohsin Syed <mohsin.syed.ms@gmail.com>
2024-01-09 17:57:01 -0800

    Initialize repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capstone 2 for springboard course. Movie Recommendation System.',
+    author='Mohsin Syed',
+    license='',
+)
